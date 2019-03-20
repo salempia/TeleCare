cd3982138870c4d7f82ef6a12520c1ccf3687529
Marie Salem <msalem@balglobal.com>
2019-03-19 18:37:05 -0700

    added Employees

--- TeleCare/TeleCare/Models/Cars.cs
+++ TeleCare/TeleCare/Models/Cars.cs
@@ -15,8 +15,7 @@ namespace TeleCare.Models
         public int CarId { get; set; }
         public string Price { get; set; }
         public string Color { get; set; }
-        public string SafetyRating { get; set; }
-        public string AddressLine2 { get; set; }
+        public string SafetyRating { get; set; }        
 
         public virtual Cars Factory { get; set; }
         public virtual ICollection<ExtraCarFeatures> ExtraCarFeatures { get; set; }
---
+++ TeleCare/TeleCare/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeleCare.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            
+        }
+
+        public int? EmployeeId { get; set; }
+
+        private string EmployeeName { get; set; }    
+
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        
+    }
+}
